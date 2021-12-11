a3ceabc24ad35ffb5a4a46730fbf292520211173
Mclilzee <mc.lilzee@hotmail.com>
2021-12-11 08:46:45 +0100

    Add background and more details

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+let playerScore = 0;
+let computerScore = 0;
+let games = 0;
+
+document.querySelector(".player-score").textContent = "Human Score: " + playerScore;
+document.querySelector(".computer-score").textContent = "Robot Score: " + computerScore;
+document.querySelector(".games-played").textContent = "Games: " + games;
+
 function computerPlay() {
   let number = Math.floor(Math.random() * 3);
 
@@ -39,34 +47,50 @@ function play(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let player = 0;
-  let computer = 0;
-  for (let i = 0; i < 5; i++) {
-    let playerChose = prompt("Choose: Rock, Paper, Scissors");
-    playerChose =
-      playerChose[0].toUpperCase() + playerChose.slice(1).toLowerCase();
-    let computerChose = computerPlay();
-    console.log("Player: " + playerChose);
-    console.log("Computer: " + computerChose);
-    let result = play(playerChose, computerChose);
-
-    if (result === 1) {
-      player++;
-    } else if (result === -1) {
-      computer++;
-    }
-
-    console.log("Player score: " + player);
-    console.log("Computer score: " + computer);
-  }
-  if (player > computer) {
-    console.log("Player Won!");
-  } else if (computer > player) {
-    console.log("Computer Won!");
-  } else {
-    console.log("It's a Draw!");
-  }
+if (games < 5) {
+  document.querySelectorAll("button").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      console.log(e.ID);
+    });
+  });
 }
 
-game();
+function reset() {
+  games = 0;
+  playerScore = 0;
+  computerScore = 0;
+  document.querySelector(".header").textContent =
+    " Choose rock, paper, scissors to start.";
+  document.querySelector(".winner").textContent = "";
+}
+
+// function game() {
+//   let player = 0;
+//   let computer = 0;
+//   for (let i = 0; i < 5; i++) {
+//     let playerChose = prompt("Choose: Rock, Paper, Scissors");
+//     playerChose =
+//       playerChose[0].toUpperCase() + playerChose.slice(1).toLowerCase();
+//     let computerChose = computerPlay();
+//     console.log("Player: " + playerChose);
+//     console.log("Computer: " + computerChose);
+//     let result = play(playerChose, computerChose);
+
+//     if (result === 1) {
+//       player++;
+//     } else if (result === -1) {
+//       computer++;
+//     }
+
+//     console.log("Player score: " + player);
+//     console.log("Computer score: " + computer);
+//   }
+//   if (player > computer) {
+//     console.log("Player Won!");
+//   } else if (computer > player) {
+//     console.log("Computer Won!");
+//   } else {
+//     console.log("It's a Draw!");
+//   }
+// }
+// game();
